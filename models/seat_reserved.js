f7fb0ec78a456a1d2b123bb58e8efe6937ea021f
Nina Belova <63970475+Nina0295@users.noreply.github.com>
2022-09-30 11:40:23 +0300

    Update seat_reserved.js

--- models/seat_reserved.js
+++ models/seat_reserved.js
@@ -1,22 +1,22 @@
-var mongoose=require("mongoose");
+const mongoose=require("mongoose");
 
-var seatReservedSchema=new mongoose.Schema(
+const seatReservedSchema=new mongoose.Schema(
 {
-	seat:
-	{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "seat"
-	},
-	reservation:
-	{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "reservation"
-	},
-	screening:
-	{
-		type: mongoose.Schema.Types.ObjectId,
-		ref: "screening"
-	}
+  seat:
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "seat"
+  },
+  reservation:
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "reservation"
+  },
+  screening:
+  {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "screening"
+  }
 });
 
 module.exports=mongoose.model("seatReserved",seatReservedSchema);
