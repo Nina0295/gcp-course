3c7e71a974eaf4b675e0639cd5244a68ff1feb67
Nina Belova <63970475+Nina0295@users.noreply.github.com>
2022-09-30 11:32:28 +0300

    Update admin.js

--- models/admin.js
+++ models/admin.js
@@ -1,12 +1,12 @@
-var mongoose=require("mongoose"),
-	passportLocalMongoose=require("passport-local-mongoose");
+const mongoose=require("mongoose"),
+  passportLocalMongoose=require("passport-local-mongoose");
 
-	var adminSchema=mongoose.Schema(
-	{
-		username: String,
-		password: String,
-		isAdmin: {type: Boolean,default: false}
-	});
+  const adminSchema=mongoose.Schema(
+{
+  username: String,
+  password: String,
+  isAdmin: {type: Boolean,default: false}
+});
 
-	adminSchema.plugin(passportLocalMongoose);
-	module.exports=mongoose.model("Admin",adminSchema);
+adminSchema.plugin(passportLocalMongoose);
+module.exports=mongoose.model("Admin",adminSchema);
