cb7551321eb7033f862957db428b32cb881735b8
Nina Belova <63970475+Nina0295@users.noreply.github.com>
2022-09-30 11:35:28 +0300

    Update movies.js

--- models/movies.js
+++ models/movies.js
@@ -1,20 +1,17 @@
-
-var mongoose=require("mongoose");
-var movieSchema=new mongoose.Schema(
+const mongoose=require("mongoose");
+const movieSchema=new mongoose.Schema(
 {
-	name: String,
-	img: String,
-	ratings: String,
-	director: String,
-	summary: String,
-	release: Number,
-	duration: Number,
-	screening: {
-		type: Number,
-		default: 0
-	}
-	
+  name: String,
+  img: String,
+  ratings: String,
+  director: String,
+  summary: String,
+  release: Number,
+  duration: Number,
+  screening: {
+    type: Number,
+    default: 0
+}	
 });
-
 
 module.exports=mongoose.model("movies",movieSchema);
