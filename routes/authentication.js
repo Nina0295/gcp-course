aea8a2c1e1375063d8bbc16df7f25cdfdf71711c
Nina Belova <63970475+Nina0295@users.noreply.github.com>
2022-09-30 12:19:57 +0300

    Update authentication.js

--- routes/authentication.js
+++ routes/authentication.js
@@ -60,8 +60,8 @@ router.get("/admin/logout",function(req,res)
 function isAdmin(req,res,next) {
   if(req.isAuthenticated()&&req.user.isAdmin==true)
     return next();
-    req.flash("error","You Must be signed in as Admin");
-    res.redirect("/admin/login");
+  req.flash("error","You Must be signed in as Admin");
+  res.redirect("/admin/login");
 }
 
 module.exports=router;
