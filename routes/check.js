f85fda2726fb814552b7a72368479cd2ca3defc7
Nina Belova <63970475+Nina0295@users.noreply.github.com>
2022-09-30 12:01:27 +0300

    Update check.js

--- routes/check.js
+++ routes/check.js
@@ -1,19 +1,13 @@
-var express=require("express"),
-	router=express.Router(),
-	mongoose=require("mongoose"),
-	reservation=require("../models/reservation");
+const express=require("express"),
+  router=express.Router(),
+  mongoose=require("mongoose"),
+  reservation=require("../models/reservation");
 
-	router.post("/check",function(req,res)
-	{
-		var mob_num=req.body.srch;
-		reservation.find({"contact":mob_num}).populate("screening_id").exec(function(err,found)
-		{
-			res.render("./search/status",{ticket:found});
-			console.log(found);
-		});
-		
-		
-	});
-
+router.post("/check",function(req,res) {
+  const mob_num=req.body.srch;
+  reservation.find({"contact":mob_num}).populate("screening_id").exec(function(err,found) {
+    res.render("./search/status",{ticket:found});
+  });
+});
 
 module.exports=router;
